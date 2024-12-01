ee8b6e2c9c2bd59bc78829bb57a853dcc9dfb133
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-30 20:58:16 -0600

    Update submit.php

--- submit.php
+++ submit.php
@@ -3,9 +3,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $title = htmlspecialchars($_POST['title']);
     $cover = htmlspecialchars($_POST['cover']);
     $description = htmlspecialchars($_POST['description']);
-    echo "<h1>Thank You for Submitting!</h1>";
-    echo "<p>Your favorite comic, $title, has been added!</p>";
-    echo "<a href='index.php'>Go Back to Gallery</a>";
+    echo "<h1>Thank You, Hero!</h1>";
+    echo "<p>You’ve added <strong>$title</strong> to the multiverse!</p>";
+    echo "<a href='index.php'>Return to Gallery</a>";
 } else {
     header('Location: index.php');
     exit;
