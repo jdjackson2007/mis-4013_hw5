a12407b3651870ec0deabd2a0d147cee44ed9a2b
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-30 19:42:22 -0600

    Create comic-data.php

---
+++ comic-data.php
@@ -0,0 +1,7 @@
+<?php
+$comics = [
+    ['title' => 'Spider-Man', 'cover' => 'https://example.com/spiderman.jpg', 'description' => 'The adventures of Peter Parker as Spider-Man.'],
+    ['title' => 'Batman', 'cover' => 'https://example.com/batman.jpg', 'description' => 'The dark knight of Gotham City.'],
+    ['title' => 'Superman', 'cover' => 'https://example.com/superman.jpg', 'description' => 'The story of Clark Kent, the Man of Steel.']
+];
+?>
