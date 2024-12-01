b9cbd2537f881a66c225ca4e87b05c518f7bdd7a
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-30 20:13:35 -0600

    Update index.php

--- index.php
+++ index.php
@@ -9,12 +9,20 @@ include 'comics-data.php';
     <title>DC Comics Universe</title>
     <link rel="stylesheet" href="assets/styles.css">
     <script src="assets/scripts.js" defer></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/animejs/3.2.1/anime.min.js" integrity="sha512-QfOTYOZ5jHkgk8X6klZtK4fiIXDxYr7/o4aR/hPlR1joUxlQU9u1Bdd5Oti89HK5U6jvpPOjh/mYT58UMczvhA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+        <script src="https://cdn.jsdelivr.net/npm/chart.js" integrity="sha384-DbaKKJFzEObzUXzzW2ud9EpMrUwUhj4fx5QWW2FOhmRMSAaE2PgaEBISpIk9P6Ok" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11" integrity="sha384-L9WbbNmIA63n/RtFE8IfX+3gPFrkRUm0R3QtNssI4guAjTAsHCUq1ewsd8nlmODN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.21/lodash.min.js" integrity="sha512-35UG0d6CrK7gwyBphAjGGgf3gNqwhjULvj3M6qPVoGpOohBrpnUAbBHbFT+T+2kvLRtNDFnghLyjmOeYdGNm7A==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
 </head>
 <body>
     <header>
         <h1>DC Comics Universe</h1>
         <button onclick="changeTheme()">Toggle Dark Mode</button>
         <button onclick="toggleGallery()">Show/Hide Gallery</button>
+
+            <canvas id="comicStats" width="400" height="200"></canvas>
+
     </header>
     <main>
         <section id="comic-gallery">
