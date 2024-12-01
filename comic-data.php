9e097e0aa5ce84bb3b510ed38e721930703901e5
jdjackson2007 <142919908+jdjackson2007@users.noreply.github.com>
2024-11-30 20:42:53 -0600

    Update comic-data.php

--- comic-data.php
+++ comic-data.php
@@ -1,11 +1,7 @@
 <?php
 $comics = [
-    ['title' => 'Batman', 'cover' => 'https://example.com/batman.jpg', 'description' => 'The Dark Knight of Gotham, protector of the city.'],
-    ['title' => 'Superman', 'cover' => 'https://example.com/superman.jpg', 'description' => 'The Man of Steel, defender of Earth.'],
-    ['title' => 'Wonder Woman', 'cover' => 'https://example.com/wonderwoman.jpg', 'description' => 'The Amazon princess, champion of peace and justice.'],
-    ['title' => 'Animal Man', 'cover' => 'https://example.com/animalman.jpg', 'description' => 'A lesser-known hero who can borrow the abilities of animals.'],
-    ['title' => 'The Question', 'cover' => 'https://example.com/question.jpg', 'description' => 'A faceless detective unraveling mysteries.'],
-    ['title' => 'Ambush Bug', 'cover' => 'https://example.com/ambushbug.jpg', 'description' => 'A quirky hero who often breaks the fourth wall.'],
-    ['title' => 'Swamp Thing', 'cover' => 'https://example.com/swampthing.jpg', 'description' => 'A protector of nature, connected to The Green.']
+    ['title' => 'Spider-Man', 'cover' => 'https://example.com/spiderman.jpg', 'description' => 'The adventures of Peter Parker as Spider-Man.'],
+    ['title' => 'Batman', 'cover' => 'https://example.com/batman.jpg', 'description' => 'The dark knight of Gotham City.'],
+    ['title' => 'Superman', 'cover' => 'https://example.com/superman.jpg', 'description' => 'The story of Clark Kent, the Man of Steel.']
 ];
 ?>
